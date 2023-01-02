895b8d47d1cb847fe55d36c510cbfd67d9c44fd4
Deviate-DV8 <88654163+Danvictorgithub@users.noreply.github.com>
2023-01-02 19:48:25 +0800

    Added User Schema

---
+++ models/user.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+	username:{type:String, required:true, minLength:1,maxLength:25},
+	password:{type:String, required:true, minLength:1,maxLength:25},
+});
+module.exports = mongoose.model("User",UserSchema);
