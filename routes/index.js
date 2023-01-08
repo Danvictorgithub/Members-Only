8bbff923b89fc90617ae4dfed68f29136bfd5b65
Deviate-DV8 <88654163+Danvictorgithub@users.noreply.github.com>
2023-01-08 19:48:17 +0800

    fixed session bugs

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const router = express.Router();
 const authenticationController = require('../controllers/authenticationController');
 router.get("/", (req,res,next) => {
-	console.log(req.session.error);
-	res.render("home",{errors:req.session.error});
-	// req.session.error = undefined;
+	// setup sign-up/login errors
+	if (typeof req.session.error != 'undefined') {
+		let errors = req.session.error;
+		delete req.session.error;
+		res.render("home",{errors:errors});
+	}
+	res.render("home");
 });
 router.post("/sign-up",authenticationController.sign_up_post);
 module.exports = router;
